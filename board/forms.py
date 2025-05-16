699b128a7aacbedb0141bd09bc4a19a93942545f
tombo314 <u_tombo@icloud.com>
2025-05-17 01:57:22 +0900

    検索機能を追加

--- board/forms.py
+++ board/forms.py
@@ -5,8 +5,11 @@ class PostForm(forms.ModelForm):
     """投稿フォーム"""
     class Meta:
         model = Post
-        fields = ('name', 'message')
+        fields = ('title', 'content')
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'あなたの名前'}),
-            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'メッセージを入力してください', 'rows': 5}),
-        } 
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'タイトルを入力してください'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'メッセージを入力してください', 'rows': 5}),
+        }
+
+class SearchForm(forms.Form):
+    query = forms.CharField(label='検索', max_length=100) 
